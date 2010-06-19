0d8c7186368dceadee41facf49037050b398afda
alkemann <alek.morland@gmail.com>
2010-06-20 01:18:06 +0200

    Added PhpFaker

---
+++ config/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ *
+ *
+ */
+
+use \lithium\core\Libraries;
+
+Libraries::add('phpfaker', array(
+	'path' => '/home/alek/www/common/dummy_data/libraries/phpfaker',
+	'bootstrap' => 'dummy_wrapper.php'
+));
+
