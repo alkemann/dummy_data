c1269e86733d7f985a74c943f816da2159512a0e
Alexander Morland <alek@redpill-linpro.com>
2011-06-10 11:10:20 +0200

    updated routing rule to use 'library' instead of 'plugin'

--- config/routes.php
+++ config/routes.php
@@ -5,9 +5,12 @@
 
 use \lithium\net\http\Router;
 
-Router::connect('/dummy_data/{:controller}/{:action}/{:args}', array(
-	'plugin' => 'dummy_data',
-	'controller' => 'models',
-	'action' => 'index'
-), array('persist' => array('plugin','controller')));
-?>
+Router::connect('/dummy/{:controller}/{:action}/{:args}', array(
+		'library' => 'dummy_data',
+		'controller' => 'models',
+		'action' => 'index'
+	), 
+	array('persist' => array('library', 'controller'))
+);
+
+/**/
